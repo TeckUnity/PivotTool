337bc9c011001f4c0acad3cd478509937ac9ea68
Teck <lotekk78@gmail.com>
2019-07-03 17:53:56 +0800

    Add edge snapping, lots of cleanup.

--- Editor/PivotTool.cs
+++ Editor/PivotTool.cs
@@ -22,10 +22,6 @@ namespace uTools
         private Quaternion pivotRotation;
         private Vector3 pivotScale;
         private Transform rotationReference;
-        // Consider PivotMode enum instead of bools?
-        // Might make multikey situations trickier to handle, at least in code
-        // eg. Hold P, press S, release either.
-        // Restore to the other mode? Restore to None?
         private bool snapPivot;
         private bool adjustPivot;
         private List<TransformEntry> selectedObjects = new List<TransformEntry>();
@@ -33,9 +29,11 @@ namespace uTools
         private SceneView view;
         private Ray ray;
         private RaycastHit hit;
+        private object hitTemp;
         private MeshFilter currentMeshFilter;
-        private Vector3[] currentVerts;
         private Edge[] currentEdges;
+        private Vector3 adjustedPoint;
+        private bool snapped;
 
         private KeyCode pivotKey;
         private KeyCode snapKey;
@@ -47,11 +45,13 @@ namespace uTools
         {
             public Vector3 v0;
             public Vector3 v1;
+            public Vector3 n;
 
-            public Edge(Vector3 _v0, Vector3 _v1)
+            public Edge(Vector3 _v0, Vector3 _v1, Vector3 _n)
             {
                 v0 = _v0;
                 v1 = _v1;
+                n = _n;
             }
         }
 
@@ -155,16 +155,15 @@ namespace uTools
             pivot = new GameObject("Pivot").transform;
             pivot.gameObject.hideFlags = HideFlags.HideAndDontSave;
 
-            // Was going to use this to be a bit flexible about delete detection, but switch/case requires constants
-            // var deleteBinding = ShortcutManager.instance.GetShortcutBinding("Main Menu/Edit/Delete").keyCombinationSequence;
-            // if (deleteBinding.Count() > 0)
-            // {
-            //     deleteKeycode = deleteBinding.First().keyCode;
-            // }
-            // else
-            // {
-            //     deleteKeycode = KeyCode.Delete;
-            // }
+            var deleteBinding = ShortcutManager.instance.GetShortcutBinding("Main Menu/Edit/Delete").keyCombinationSequence;
+            if (deleteBinding.Count() > 0)
+            {
+                deleteKeycode = deleteBinding.First().keyCode;
+            }
+            else
+            {
+                deleteKeycode = KeyCode.Delete;
+            }
 
             OnSelectionChange();
         }
@@ -268,45 +267,49 @@ namespace uTools
 
             for (int i = 0; i < mesh.triangles.Length; i += 3)
             {
-                var v1 = mf.transform.TransformPoint(mesh.vertices[mesh.triangles[i]]);
-                var v2 = mf.transform.TransformPoint(mesh.vertices[mesh.triangles[i + 1]]);
-                var v3 = mf.transform.TransformPoint(mesh.vertices[mesh.triangles[i + 2]]);
-                edges.Add(new Edge(v1, v2));
-                edges.Add(new Edge(v1, v3));
-                edges.Add(new Edge(v2, v3));
+                var v0 = mf.transform.TransformPoint(mesh.vertices[mesh.triangles[i]]);
+                var v1 = mf.transform.TransformPoint(mesh.vertices[mesh.triangles[i + 1]]);
+                var v2 = mf.transform.TransformPoint(mesh.vertices[mesh.triangles[i + 2]]);
+                var n0 = mf.transform.TransformDirection(mesh.normals[mesh.triangles[i]] + mesh.normals[mesh.triangles[i + 1]]).normalized;
+                var n1 = mf.transform.TransformDirection(mesh.normals[mesh.triangles[i]] + mesh.normals[mesh.triangles[i + 2]]).normalized;
+                var n2 = mf.transform.TransformDirection(mesh.normals[mesh.triangles[i + 1]] + mesh.normals[mesh.triangles[i + 2]]).normalized;
+                edges.Add(new Edge(v0, v1, n0));
+                edges.Add(new Edge(v0, v2, n1));
+                edges.Add(new Edge(v1, v2, n2));
             }
 
             return edges.ToArray();
         }
 
 
-        private bool Raycast()
+        private bool Raycast(bool forceRefresh = false)
         {
-            GameObject g = HandleUtility.PickGameObject(e.mousePosition, false);
-            if (g)
+            Vector2 mousePosition = view.camera.ScreenToViewportPoint(e.mousePosition * EditorGUIUtility.pixelsPerPoint);
+            mousePosition.y = 1 - mousePosition.y;
+            ray = view.camera.ViewportPointToRay(mousePosition);
+            hitTemp = HandleUtility.RaySnap(ray);
+            if (hitTemp == null)
             {
-                MeshFilter mf = g.GetComponent<MeshFilter>();
+                hit = new RaycastHit();
+                currentEdges = null;
+                hit = new RaycastHit();
+                return false;
+            }
+            hit = (RaycastHit)hitTemp;
+            if (hit.transform)
+            {
+                MeshFilter mf = hit.transform.GetComponent<MeshFilter>();
                 if (mf)
                 {
-                    if (currentMeshFilter != mf)
+                    if (currentMeshFilter != mf || forceRefresh)
                     {
                         currentMeshFilter = mf;
-                        currentVerts = new Vector3[mf.sharedMesh.vertexCount];
-                        for (int i = 0; i < currentVerts.Length; i++)
-                        {
-                            currentVerts[i] = mf.transform.TransformPoint(mf.sharedMesh.vertices[i]);
-                        }
                         currentEdges = GetMeshEdges(currentMeshFilter);
                     }
-                    Vector2 mousePosition = view.camera.ScreenToViewportPoint(e.mousePosition * EditorGUIUtility.pixelsPerPoint);
-                    mousePosition.y = 1 - mousePosition.y;
-                    ray = view.camera.ViewportPointToRay(mousePosition);
-                    if (MeshRaycast.IntersectRayMesh(ray, mf, out hit))
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
+            currentEdges = null;
             hit = new RaycastHit();
             return false;
         }
@@ -331,94 +334,105 @@ namespace uTools
             }
             if (e.type == EventType.KeyDown)
             {
-                switch (e.keyCode)
+                // Prevent object deletion, really only because of the following error:
+                // "Nested object batch deleting detected!"
+                if (e.keyCode == deleteKeycode)
                 {
-                    case KeyCode.Delete:
-                        // Prevent object deletion, really only because of the following error:
-                        // "Nested object batch deleting detected!"
-                        e.Use();
-                        break;
+                    e.Use();
                 }
             }
+            if ((!snapPivot && snapKey != KeyCode.None && e.type == EventType.KeyDown && e.keyCode == snapKey) ||
+                (snapPivot && (e.type == EventType.MouseMove || e.type == EventType.MouseDrag)))
+            {
+                Raycast();
+            }
+            snapped = false;
             if (snapPivot)
             {
                 // Ensure clicking in snap mode doesn't select the object under the pointer
                 HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
-                if (currentMeshFilter)
+                if (currentEdges != null)
                 {
-                    float d = Mathf.Infinity;
-                    int index = -1;
+                    Vector3[] edgePoints = new Vector3[3];
                     Vector3 p = view.camera.WorldToScreenPoint(hit.point);
-                    for (int i = 0; i < currentVerts.Length; i++)
+                    Vector3 p1 = p;
+                    adjustedPoint = hit.point;
+                    Edge edge = currentEdges.Where(e => Vector3.Dot(e.n, ray.direction) < 0).OrderBy(e => HandleUtility.DistancePointLine(hit.point, e.v0, e.v1)).First();
+                    float d = HandleUtility.DistancePointLine(hit.point, edge.v0, edge.v1);
+                    edgePoints[0] = edge.v0;
+                    edgePoints[1] = (edge.v0 + edge.v1) / 2;
+                    edgePoints[2] = edge.v1;
+                    Vector3 cp = edgePoints.OrderBy(v => (view.camera.WorldToScreenPoint(v) - p).sqrMagnitude).First();
+                    if ((view.camera.WorldToScreenPoint(cp) - p).sqrMagnitude < 25 * 25)
                     {
-                        Handles.DotHandleCap(0, currentVerts[i], Quaternion.identity, HandleUtility.GetHandleSize(currentVerts[i]) * 0.05f, EventType.Repaint);
-                        float d2 = (view.camera.WorldToScreenPoint(currentVerts[i]) - p).sqrMagnitude;
-                        if (d2 < d)
+                        p1 = cp;
+                    }
+                    else
+                    {
+                        p1 = HandleUtility.ProjectPointLine(hit.point, edge.v0, edge.v1);
+                    }
+                    if (d < 0.1f)
+                    {
+                        if ((view.camera.WorldToScreenPoint(p1) - p).sqrMagnitude < 25 * 25)
                         {
-                            d = d2;
-                            index = i;
+                            Handles.zTest = UnityEngine.Rendering.CompareFunction.Less;
+                            Color c = Handles.color;
+                            Handles.color = Color.cyan;
+                            Handles.DrawAAPolyLine(5, edge.v0, edge.v1);
+                            Handles.color = c;
+                            Handles.zTest = UnityEngine.Rendering.CompareFunction.Always;
+                            adjustedPoint = p1;
+                            snapped = true;
                         }
                     }
-                    if (d < 25 * 25)
-                    {
-                        hit.point = currentVerts[index];
-                    }
-                    // index = -1;
-                    // for (int i = 0; i < currentEdges.Length; i++)
-                    // {
-                    //     p = HandleUtility.ClosestPointToPolyLine(currentEdges[i].v0, currentEdges[i].v1);
-                    //     float d2 = (p - hit.point).sqrMagnitude;
-                    //     if (d2 < d)
-                    //     {
-                    //         index = i;
-                    //         d = d2;
-                    //     }
-                    // }
-                    // if (d < 0.1f * 0.1f)
-                    // {
-                    //     hit.point = p;
-                    // }
                 }
-            }
-            if ((!snapPivot && snapKey != KeyCode.None && e.type == EventType.KeyDown && e.keyCode == snapKey) ||
-                (snapPivot && e.type == EventType.MouseMove))
-            {
-                Raycast();
             }
             if (snapPivot && e.type == EventType.MouseUp && e.button == 0)
             {
-                // if (Raycast())
+                if (Raycast())
                 {
                     Undo.RecordObjects(selectedObjects.Select(o => o.Transform).ToArray(), "Move");
                     Undo.RecordObjects(selectedObjects.Select(o => o.Pivot).ToArray(), "Move");
                     Undo.RecordObject(pivot, "Move");
-                    pivot.position = pivotPosition = hit.point;
+                    pivot.position = pivotPosition = adjustedPoint;
                     foreach (var entry in selectedObjects)
                     {
                         if (adjustPivot)
                         {
-                            // entry.Position = entry.Pivot.position;
                             entry.Pivot.position = entry.Position;
-                            // entry.Transform.position = entry.Position;
                         }
                         else
                         {
                             entry.Transform.position = entry.Pivot.position;
+                            Raycast(forceRefresh: true);
                         }
                     }
                     return;
                 }
             }
-            if (snapPivot && e.type == EventType.Repaint && hit.distance > 0)
+            if (snapPivot && hit.distance > 0)
             {
-                Color c = Handles.color;
-                Handles.color = Color.white * 0.9f;
-                Handles.SphereHandleCap(0, hit.point, Quaternion.identity, HandleUtility.GetHandleSize(hit.point) * 0.1f, EventType.Repaint);
-                Handles.color = Color.white * 0.5f;
-                Handles.SphereHandleCap(0, hit.point, Quaternion.identity, view.camera.farClipPlane / 100000, EventType.Repaint);
-                Handles.color = c;
+                if (e.type == EventType.Repaint)
+                {
+                    Color c = Handles.color;
+                    Handles.CubeHandleCap(0, pivot.position, pivot.rotation, HandleUtility.GetHandleSize(pivot.position) * 0.1f, EventType.Repaint);
+                    Handles.color = Handles.xAxisColor;
+                    Handles.DrawLine(pivot.position, pivot.position + (pivot.right * HandleUtility.GetHandleSize(pivot.position)));
+                    Handles.color = Handles.yAxisColor;
+                    Handles.DrawLine(pivot.position, pivot.position + (pivot.up * HandleUtility.GetHandleSize(pivot.position)));
+                    Handles.color = Handles.zAxisColor;
+                    Handles.DrawLine(pivot.position, pivot.position + (pivot.forward * HandleUtility.GetHandleSize(pivot.position)));
+                    Handles.color = (snapped ? Color.green : Color.white) * 0.9f;
+                    Handles.SphereHandleCap(0, adjustedPoint, Quaternion.identity, HandleUtility.GetHandleSize(adjustedPoint) * 0.1f, EventType.Repaint);
+                    Handles.color = Color.white * 0.5f;
+                    Handles.SphereHandleCap(0, adjustedPoint, Quaternion.identity, view.camera.farClipPlane / 100000, EventType.Repaint);
+                    Handles.color = c;
+                }
             }
-            Handles.TransformHandle(ref pivotPosition, ref pivotRotation, ref pivotScale);
+            else
+            {
+                Handles.TransformHandle(ref pivotPosition, ref pivotRotation, ref pivotScale);
+            }
             if (EditorGUIUtility.hotControl != 0)
             {
                 int handleId = EditorGUIUtility.hotControl + 15 - GetLastControlId();
